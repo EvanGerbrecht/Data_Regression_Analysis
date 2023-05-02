08b508606040d5e0a8eb83fb2d2d5e29011747b8
Gerbrecht <evancg@ead.ubc.ca>
2023-05-02 09:26:47 -0700

    Deleting of extra files

--- scripts/aba_test.R
+++
@@ -1,7 +0,0 @@
-library(lidR)
-
-## Load LiDAR Tiles
-gc1 <- readLAScatalog("E:/Lidar_M300/05_15_22_GC1/04_classified_all/")
-
-metrics_gc1_w2w <- pixel_metrics(gc1, .stdmetrics_z, res = 20, pkg = 'terra')
- 
