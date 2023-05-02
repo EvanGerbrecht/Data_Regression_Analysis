da460e3cc08ee2ac5049dd1b78221d3eb9e9a41b
Gerbrecht <evancg@ead.ubc.ca>
2023-05-02 14:07:52 -0700

    updated grid_metrics

--- area.based_canopy.fuel.R
+++ area.based_canopy.fuel.R
@@ -16,22 +16,29 @@ df <- regression_data
 df <- df %>%
   mutate(zq95 = rpas_zq95, zmax = plot_max)
 
+gc1_filter <- filter_poi(gc1, Z<=40)
+
 # Retrieve grid metrics (standard_z) from the different flights, 20m resolution 
-metrics_gc1_w2w <- grid_metrics(gc1, .stdmetrics_z, res = 20)
-metrics_gc2_w2w <- grid_metrics(gc2, .stdmetrics_z, res = 20)
+metrics_gc1_w2w <- grid_metrics(gc1, .stdmetrics_z, res = 20, filter = ~Z>=0 & Z<=40)
+metrics_gc2_w2w <- grid_metrics(gc2, .stdmetrics_z, res = 20, filter = ~Z>=0 & Z<=40)
 
 # Linear basic regression model
 fuel_max <- lm(canopy_fuel ~ zmax, data = df)
 summary(fuel_max)
 
 # Use basic linear model to predict Canopy Fuel Load
-cfl_pred <- raster::predict(metrics_gc1_w2w$zmax, fuel_max)
+cfl_pred_test <- raster::predict(metrics_gc1_w2w_test$zmax, fuel_max)
 cfl_pred_2 <- raster::predict(metrics_gc2_w2w$zmax, fuel_max)
 
+# Plot and visualize the the LIDAR metrics
+plot(metrics_gc1_w2w_test$zmax)
+hist(metrics_gc1_w2w_test$zmax) # few outliers above heights of 40m, need to clip those out and re-do
+
 # Plot and visualize the results of CFL prediction
-plot(metrics_gc1_w2w$zmax)
-plot(cfl_pred_2)
+plot(cfl_pred_test)
 plot(cfl_pred)
+
+hist(cfl_pred_test)
 
 # Added legend for when sending results to people
 plot(cfl_pred, legend.args = list(text = 'CFL (kg)', side = 4, 
